8480d581cf8b420da0ee041c992fde9cda213909
O-Will-O <winderswilliam122205@gmail.com>
2024-04-08 10:00:47 -0400

    add stuff

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from sqlalchemy import create_engine, text
 app = Flask(__name__)
 
 # connection string is in the format mysql://user:password@server/database
-conn_str = "mysql://root:cyber241@localhost/160final"
+conn_str = "mysql://root:Ilikegames05!@localhost/160final"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
